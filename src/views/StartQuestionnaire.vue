3183e2d548481ae939780a5ef188d2509269d580
tazoakhalaia <darknemiroff@gmail.com>
2023-06-13 15:43:56 +0400

    fix/issues

--- src/views/StartQuestionnaire.vue
+++ src/views/StartQuestionnaire.vue
@@ -3,7 +3,7 @@
     <center>
       <div class="absolute top-1/3 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
         <transition name="image">
-          <img v-if="mainImageshow" class="w-28 h-28" src="../assets/image/start.png" />
+          <StartQuestionnaireImage class="w-28 h-28" v-if="mainImageshow" />
         </transition>
       </div>
     </center>
@@ -21,6 +21,7 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import StartQuestionnaireImage from '../assets/image/StartQuestionnaireImage.vue'
 let mainImageshow = ref(false)
 let startText = ref(false)
 onMounted(() => {
@@ -28,37 +29,11 @@ onMounted(() => {
     mainImageshow.value = true
     setTimeout(() => {
       startText.value = true
-    }, 2000)
+    }, 1000)
   } else {
     mainImageshow.value = false
   }
 })
 </script>
 
-<style scoped>
-.image-enter-from {
-  width: 100%;
-  height: 100%;
-  transform: scale(5);
-}
-
-.image-enter-to {
-  width: 112px;
-  height: 112px;
-}
-
-.image-enter-active {
-  transition: all 0.5s ease-in-out;
-}
-
-.start-enter-from {
-  opacity: 0;
-}
-
-.start-enter-to {
-  opacity: 1;
-}
-.start-enter-active {
-  transition: all 2s ease-in-out;
-}
-</style>
+<style scoped src="../assets/css/startquestionnaire.css"></style>
