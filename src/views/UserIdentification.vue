b2eafe9b7d8d603eafe1d2e1922386810a8c3d97
tazoakhalaia <darknemiroff@gmail.com>
2023-06-16 18:58:49 +0400

    add header

---
+++ src/views/UserIdentification.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-screen h-screen flex justify-center items-center">
+    <div class="w-11/12 h-5/6">
+      <PageHeader :pageNumber="firstPage" />
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from 'vue'
+import PageHeader from '@/components/shared/PageHeader.vue'
+let firstPage = ref(1)
+</script>
